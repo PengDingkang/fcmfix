3e98cdb677627812d1b7623bab0d4ba795ab21ea
Nagisa_Cheese <mew@mevv.moe>
2026-03-31 23:21:50 +0800

    fix powerkeeper hook

--- app/src/main/java/com/kooritea/fcmfix/xposed/PowerkeeperFix.java
+++ app/src/main/java/com/kooritea/fcmfix/xposed/PowerkeeperFix.java
@@ -18,76 +18,110 @@ public class PowerkeeperFix extends XposedModule {
     }
 
     protected void startHook(){
+        // MilletConfig.isGlobal - may not exist on newer HyperOS
         try {
-
             Class<?> MilletConfig = XposedHelpers.findClassIfExists("com.miui.powerkeeper.millet.MilletConfig", loadPackageParam.classLoader);
-            XposedHelpers.setStaticBooleanField(MilletConfig, "isGlobal", true);
-            printLog("Set com.miui.powerkeeper.millet.MilletConfig.isGlobal to true");
+            if (MilletConfig != null) {
+                XposedHelpers.setStaticBooleanField(MilletConfig, "isGlobal", true);
+                printLog("Set com.miui.powerkeeper.millet.MilletConfig.isGlobal to true");
+            } else {
+                printLog("MilletConfig not found, skip isGlobal patch");
+            }
+        } catch (Throwable e) {
+            printLog("hook error MilletConfig.isGlobal: " + e.getMessage());
+        }
 
+        // SimpleSettings.Misc.getBoolean -> gms_control
+        try {
             Class<?> Misc = XposedHelpers.findClassIfExists("com.miui.powerkeeper.provider.SimpleSettings.Misc", loadPackageParam.classLoader);
-            printLog("[fcmfix] start hook com.miui.powerkeeper.provider.SimpleSettings.Misc.getBoolean");
-            XposedUtils.findAndHookMethod(Misc, "getBoolean", 3, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                    if("gms_control".equals((String) methodHookParam.args[1])) {
-                        printLog("Success: Success: PowerKeeper GMS Limitation. ", true);
-                        methodHookParam.setResult(false);
-                    }
-                }
-            });
-
-            Class<?> MilletPolicy = XposedHelpers.findClassIfExists("com.miui.powerkeeper.millet.MilletPolicy", loadPackageParam.classLoader);
-
-            XC_MethodHook methodHook = new XC_MethodHook() {
-                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
-                    Field[] declaredFields = null;
-                    super.afterHookedMethod(methodHookParam);
-                    boolean mSystemBlackList = false;
-                    boolean whiteApps = false;
-                    boolean mDataWhiteList = false;
-
-                    for (Field field : MilletPolicy.getDeclaredFields()) {
-                        if (field.getName().equals("mSystemBlackList")) {
-                            mSystemBlackList = true;
-                        } else if (field.getName().equals("whiteApps")) {
-                            whiteApps = true;
-                        } else if (field.getName().equals("mDataWhiteList")) {
-                            mDataWhiteList = true;
+            if (Misc != null) {
+                printLog("[fcmfix] start hook com.miui.powerkeeper.provider.SimpleSettings.Misc.getBoolean");
+                XposedUtils.findAndHookMethod(Misc, "getBoolean", 3, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        if("gms_control".equals((String) methodHookParam.args[1])) {
+                            printLog("Success: PowerKeeper GMS Limitation disabled.", true);
+                            methodHookParam.setResult(false);
                         }
                     }
+                });
+            } else {
+                printLog("SimpleSettings.Misc not found, skip gms_control hook");
+            }
+        } catch (Throwable e) {
+            printLog("hook error SimpleSettings.Misc.getBoolean: " + e.getMessage());
+        }
 
-                    if (mSystemBlackList) {
-                        List blackList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "mSystemBlackList");
-                        blackList.remove("com.google.android.gms");
-                        XposedHelpers.setObjectField(methodHookParam.thisObject, "mSystemBlackList", blackList);
-                        printLog("Success: MilletPolicy mSystemBlackList.");
-                    } else {
-                        printLog("Error: MilletPolicy. Field not found: com.miui.powerkeeper.millet.MilletPolicy.mSystemBlackList");
+        // GmsObserver.isGmsControlEnabled - new HyperOS fallback
+        try {
+            Class<?> GmsObserver = XposedHelpers.findClassIfExists("com.miui.powerkeeper.utils.GmsObserver", loadPackageParam.classLoader);
+            if (GmsObserver != null) {
+                XposedHelpers.findAndHookMethod(GmsObserver, "isGmsControlEnabled", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        param.setResult(false);
+                        printLog("Success: GmsObserver.isGmsControlEnabled -> false", true);
                     }
-                    if (whiteApps) {
-                        List whiteAppList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "whiteApps");
-                        whiteAppList.remove("com.google.android.gms");
-                        whiteAppList.remove("com.google.android.ext.services");
-                        XposedHelpers.setObjectField(methodHookParam.thisObject, "whiteApps", whiteAppList);
-                        printLog("Success: MilletPolicy whiteApps.");
-                    } else {
-                        printLog("Error: MilletPolicy. Field not found: com.miui.powerkeeper.millet.MilletPolicy.whiteApps");
+                });
+                printLog("Hooked GmsObserver.isGmsControlEnabled");
+            }
+        } catch (Throwable e) {
+            printLog("hook error GmsObserver.isGmsControlEnabled: " + e.getMessage());
+        }
+
+        // MilletPolicy constructor - may not exist on newer HyperOS
+        try {
+            Class<?> MilletPolicy = XposedHelpers.findClassIfExists("com.miui.powerkeeper.millet.MilletPolicy", loadPackageParam.classLoader);
+            if (MilletPolicy != null) {
+                XC_MethodHook methodHook = new XC_MethodHook() {
+                    protected void beforeHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
+                        super.afterHookedMethod(methodHookParam);
+                        boolean mSystemBlackList = false;
+                        boolean whiteApps = false;
+                        boolean mDataWhiteList = false;
+
+                        for (Field field : methodHookParam.thisObject.getClass().getDeclaredFields()) {
+                            if (field.getName().equals("mSystemBlackList")) {
+                                mSystemBlackList = true;
+                            } else if (field.getName().equals("whiteApps")) {
+                                whiteApps = true;
+                            } else if (field.getName().equals("mDataWhiteList")) {
+                                mDataWhiteList = true;
+                            }
+                        }
+
+                        if (mSystemBlackList) {
+                            List blackList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "mSystemBlackList");
+                            blackList.remove("com.google.android.gms");
+                            XposedHelpers.setObjectField(methodHookParam.thisObject, "mSystemBlackList", blackList);
+                            printLog("Success: MilletPolicy mSystemBlackList.");
+                        } else {
+                            printLog("Error: MilletPolicy. Field not found: mSystemBlackList");
+                        }
+                        if (whiteApps) {
+                            List whiteAppList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "whiteApps");
+                            whiteAppList.remove("com.google.android.gms");
+                            whiteAppList.remove("com.google.android.ext.services");
+                            XposedHelpers.setObjectField(methodHookParam.thisObject, "whiteApps", whiteAppList);
+                            printLog("Success: MilletPolicy whiteApps.");
+                        } else {
+                            printLog("Error: MilletPolicy. Field not found: whiteApps");
+                        }
+                        if (mDataWhiteList) {
+                            List dataWhiteList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "mDataWhiteList");
+                            dataWhiteList.add("com.google.android.gms");
+                            XposedHelpers.setObjectField(methodHookParam.thisObject, "mDataWhiteList", dataWhiteList);
+                            printLog("Success: MilletPolicy mDataWhiteList.");
+                        }
                     }
-                    if (mDataWhiteList) {
-                        List dataWhiteList = (List) XposedHelpers.getObjectField(methodHookParam.thisObject, "mDataWhiteList");
-                        dataWhiteList.add("com.google.android.gms");
-
-                        XposedHelpers.setObjectField(methodHookParam.thisObject, "mDataWhiteList", dataWhiteList);
-                        printLog("Success: MilletPolicy mDataWhiteList.");
-                    }
-
-                }
-            };
-            printLog("[fcmfix] start hook com.miui.powerkeeper.millet.MilletPolicy constructor");
-            XposedHelpers.findAndHookConstructor(MilletPolicy, new Object[] {Context.class, methodHook});
-
-        } catch (XposedHelpers.ClassNotFoundError | NoSuchMethodError  e){
-            printLog("No Such Method com.android.server.am.ProcessMemoryCleaner.checkBackgroundAppException");
+                };
+                printLog("[fcmfix] start hook com.miui.powerkeeper.millet.MilletPolicy constructor");
+                XposedHelpers.findAndHookConstructor(MilletPolicy, new Object[] {Context.class, methodHook});
+            } else {
+                printLog("MilletPolicy not found, skip blacklist/whitelist patch");
+            }
+        } catch (Throwable e) {
+            printLog("hook error MilletPolicy: " + e.getMessage());
         }
     }
 }
